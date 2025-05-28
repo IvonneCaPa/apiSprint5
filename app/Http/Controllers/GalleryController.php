e8f8c3933a466483dfeb4d7373228abb1e7191fb
Ivonne Cantor <ivonnecp@gmail.com>
2025-05-28 11:10:57 +0200

    se ha hecho el metodo index, creado el Resource para listar las galerias y creado el modelo de la tabla Gallery, se ha ejecutado el test y ahora hay que ver porque no se conecta

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\GalleryResource;
+use App\Models\Gallery;
 
 class GalleryController extends Controller
 {
     public function index()
     {
-        
+        return response([
+            'galleries'=> new GalleryResource(Gallery::all())
+        ]);
     }
 }

81c47a27fc40cc948d9e8cd1434c92cca14395fc
Ivonne Cantor <ivonnecp@gmail.com>
2025-05-28 11:27:36 +0200

    para solucionar el error que salia en el test al conectars a la base de datos, volvi a crear la funcion setUp y authenticated, además de crear la migración que no estaba hecha

--- tests/Feature/GalleryTest.php
+++ tests/Feature/GalleryTest.php
@@ -5,13 +5,42 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class GalleryTest extends TestCase
 {
+    use RefreshDatabase; 
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('passport:client', [
+            '--personal' => true,
+            '--name' => 'Testing Client',
+            '--provider' => 'users'
+        ]);
+    }
+
+    public function authenticated(){
+    $user = User::create([
+        'name'=>'garfield',
+        'email'=>rand(12345, 678910).'@info.com',
+        'role'=>User::ADMINISTRADOR,
+        'password'=>bcrypt('123456')
+    ]);
+
+    $response = $this->post(route('api.login'), [
+        'email' => $user->email,
+        'password' => '123456'
+    ]);
+
+    return $response->json()['access_token'];
+    }
     /**
      * A basic feature test example.
      */
-    public function test_can_list_galleries(){
+    public function test_galleries_can_be_retrieved()
+    {
         $this->withoutExceptionHandling();
 
         $response = $this->get(route('api.galleries.index'));

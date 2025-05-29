3d4d6ee7f32ae01f294ac47d8647a209ca6e2ad4
Ivonne Cantor <ivonnecp@gmail.com>
2025-05-29 13:49:07 +0200

    se ha puesto el return, ahora no tenemos el model

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\PhotoResource;
 
 class PhotoController extends Controller
 {
     public function index()
     {
-        
+        return response([
+            'photos' => new PhotoResource(Photo::with)
+        ]);
     }
 }

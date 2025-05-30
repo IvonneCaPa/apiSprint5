94ec6695d5aa1c45338f88436795014fa1d06ca1
Ivonne Cantor <ivonnecp@gmail.com>
2025-05-30 09:45:55 +0200

    test para agregar foto escrito, se probo y hay que habilitar la extension GD

--- tests/Feature/PhotoTest.php
+++ tests/Feature/PhotoTest.php
@@ -5,6 +5,11 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Gallery;
+use App\Models\Photo;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
 
 class PhotoTest extends TestCase
 {
@@ -13,6 +18,32 @@ class PhotoTest extends TestCase
      * A basic feature test example.
      */
     //todos
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('passport:client', [
+            '--personal' => true,
+            '--name' => 'Testing Client',
+            '--provider' => 'users'
+        ]);
+    }
+
+    public function authenticated(){
+        $user = User::create([
+            'name'=>'garfield',
+            'email'=>rand(12345, 678910).'@info.com',
+            'role'=>User::ADMINISTRADOR,
+            'password'=>bcrypt('123456')
+        ]);
+
+        $response = $this->post(route('api.login'), [
+            'email' => $user->email,
+            'password' => '123456'
+        ]);
+
+        return $response->json()['access_token'];
+    }
+
     public function test_photos_can_be_retrieved()
     {
         $this->withoutExceptionHandling();
@@ -21,5 +52,41 @@ class PhotoTest extends TestCase
         $response->assertStatus(200);
         $this->assertArrayHasKey('photos', $response->json());
     }
+
+    public function test_a_photo_can_be_uploaded()
+    {
+        $this->withoutExceptionHandling();
         
+        Storage::fake('public');
+
+        $token = $this->authenticated();
+
+        $gallery = Gallery::create([
+            'title' => 'Exposición fotos',
+            'date' => '2025-08-15',
+            'site' => 'Centro Civico X'
+        ]);
+
+        $file = UploadedFile::fake()->image('foto_test.jpg', 800, 600);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json'
+        ])->post(route('api.photo.store'), [
+            'gallery_id' => $gallery->id,
+            'title' => 'Foto de prueba',
+            'location' => $file 
+        ]);
+
+        $response->assertStatus(201);
+        
+        $this->assertCount(1, Photo::all());
+        $this->assertArrayHasKey('photo', $response->json());
+        
+        $photo = Photo::first();
+        Storage::disk('public')->assertExists($photo->location);
+        
+        $this->assertEquals($gallery->id, $photo->gallery_id);
+        $this->assertEquals('Foto de prueba', $photo->title);
+    }
 }

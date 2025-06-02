d5802a92238780e28c7f6184ad9ee1fcfa4a7652
Ivonne Cantor <ivonnecp@gmail.com>
2025-06-03 01:01:38 +0200

    en AuthTest se han creado los metodos setUp() y authenticated()

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -11,14 +11,31 @@ use Illuminate\Http\Request;
 class AuthTest extends TestCase
 {
     use RefreshDatabase;
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('passport:client', [
+            '--personal' => true,
+            '--name' => 'Testing Client',
+            '--provider' => 'users'
+        ]);
+    }
+
+    public function authenticated(){
+        $user = User::create([
+            'name'=>'garfield',
+            'email'=>rand(12345, 678910).'@info.com',
+            'role'=>User::ADMINISTRADOR,
+            'password'=>bcrypt('123456')
+        ]);
+
+        $response = $this->post(route('api.login'), [
+            'email' => $user->email,
+            'password' => '123456'
+        ]);
+
+        return $response->json()['access_token'];
     }
 
     public function test_login(){
@@ -51,24 +68,25 @@ class AuthTest extends TestCase
     }
 
     public function test_register(){
-        
-        $this->artisan('passport:client', [
-            '--personal' => true,
-            '--name' => 'Testing Client',
-            '--provider' => 'users'
-        ]);
-        
         $this->withoutExceptionHandling();
-
-        $response = $this->post(route('api.register'), [
+        
+         $userData = [
             'name'=> 'Manolita',
-            'email' => 'info@info.com',
+            'email' => 'info@info.com', 
             'role' => User::ADMINISTRADOR,
             'password' => '123456'
-        ]);
+        ];
+
+        $response = $this->post(route('api.register'), $userData);
 
         $response->assertStatus(200);
         $this->assertArrayHasKey('access_token', $response->json());
+        
+        $this->assertDatabaseHas('users', [
+            'name' => 'Manolita',
+            'email' => 'info@info.com',
+            'role' => User::ADMINISTRADOR
+        ]);
     }
 }
    

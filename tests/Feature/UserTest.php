3e5fb02d62b57fdbfe00ecdc9e8f4d83f7d5ccbd
Ivonne Cantor <ivonnecp@gmail.com>
2025-06-03 02:13:04 +0200

    test hecho se ha ejecutado y no encuentra la ruta

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -25,7 +25,7 @@ class UserTest extends TestCase
         $user = User::create([
             'name'=>'garfield',
             'email'=>rand(12345, 678910).'@info.com',
-            'role'=>User::USUARIO,
+            'role'=>User::ADMINISTRADOR,
             'password'=>bcrypt('123456')
         ]);
 
@@ -47,8 +47,6 @@ class UserTest extends TestCase
         $this->assertArrayHasKey('users', $response->json());
     }
 
-
-
     // test para recuperar un usuario
     public function test_a_user_can_be_retrieved()
     {        
@@ -65,4 +63,39 @@ class UserTest extends TestCase
         $this->assertArrayHasKey('name', $response->json());
         $this->assertArrayHasKey('email', $response->json());
     }
+
+    // test para editar un usuario
+    public function test_a_user_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        $token = $this->authenticated();
+
+        $user = User::create([
+            'name' => 'Usuario Original',
+            'email' => 'original@test.com',
+            'role' => User::USUARIO,
+            'password' => bcrypt('password123')
+        ]);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json'
+        ])->put(route('api.user.update', $user->id), [
+            'name' => 'Usuario Actualizado',
+            'email' => 'actualizado@test.com',
+            'role' => User::ADMINISTRADOR
+        ]);
+
+        $updatedUser = User::find($user->id);
+
+        $this->assertEquals($updatedUser->name, 'Usuario Actualizado');
+        $this->assertEquals($updatedUser->email, 'actualizado@test.com');
+        $this->assertEquals($updatedUser->role, User::ADMINISTRADOR);
+        
+        $this->assertArrayHasKey('user', $response->json());
+        $response->assertJsonMissing(['error']);
+        $response->assertStatus(200);
+    }
+
 }

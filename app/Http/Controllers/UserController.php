a49a68c43aace3e0856d97d8c449a7c8c346e6d7
Ivonne Cantor <ivonnecp@gmail.com>
2025-06-03 02:50:04 +0200

    metodo listo, test en verde

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Http\Resources\UserResource;
 use App\Http\Requests\UpdateUserRequest;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 
 class UserController extends Controller
@@ -31,6 +32,28 @@ class UserController extends Controller
             return response()->json([
                 'message' => 'Usuario actualizado correctamente',
                 'user' => new UserResource($user)
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response([
+                'error'=>$th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function destroy(User $user)
+    {
+        try {
+            if (Auth::id() === $user->id) {
+                return response()->json([
+                    'error' => 'No puedes eliminarte a ti mismo'
+                ], 403);
+            }
+
+            $user->delete();
+
+            return response()->json([
+                'message' => 'Usuario eliminado correctamente'
             ], 200);
 
         } catch (\Throwable $th) {
